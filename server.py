b4b4e760ec472222988b88c208a4cb8b3f2a3764
Priyom Saha <priyom_saha@Priyoms-Mac-mini.local>
2025-07-21 10:07:05 +0530

    Gold Silver Predict Telegram bot render files

--- server.py
+++ server.py
@@ -1,19 +1,21 @@
 from flask import Flask
 from main import run_daily_signal
+import os
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/predict')
 def index():
     run_daily_signal()
     return "✅ Signal sent to Telegram."
 
 
-@app.route('/alive')
+@app.route('/')
 def alive():
     return "✅ I am alive"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
